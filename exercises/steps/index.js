a26f449465431015743730811aacc03e9f7cad5c
a-soliman <ahmed.soliman@programmer.net>
2018-06-30 13:32:44 -0700

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,28 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row=0, col='') {
+    if ( n === row ) return; 
+    if ( n === col.length ) {
+        console.log(col);
+        return steps(n, row+1);
+    }
+    if ( col.length > row ) col += ' ';
+    else col += '#';
+    return steps(n, row, col);
+}
 
 module.exports = steps;
+
+
+// function steps(n) {
+//     let newLine = Array(n).fill('#').join('');
+//     let spaces = Array(n).fill(' ').join('');
+//     let times = 1;
+    
+//     while ( times <= n ) {
+//       let stringToPrint = newLine.slice(0, times).concat(spaces.slice(0, n - times));
+//       console.log(stringToPrint)
+//       times++;
+//     }
+// }
