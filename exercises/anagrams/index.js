ae46304b668476e213252fd512e85a0fc1d6f06c
a-soliman <ahmed.soliman@programmer.net>
2018-06-30 11:51:52 -0700

    isAnagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let charMap = {};
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let specialChars = [0,0];
+    for ( let i = 0; i < stringA.length; i++ ) {
+      let currentChar = stringA[i].toLowerCase();
+      if ( alphabet.indexOf(currentChar) !== -1 ) {
+        if ( !charMap[currentChar] ) charMap[currentChar] = 0;
+        charMap[currentChar] ++;
+      }
+      else {
+        specialChars[0]++;
+      }
+    }
+  
+    for ( let i = 0; i < stringB.length; i++ ) {
+      let currentChar = stringB[i].toLowerCase();
+      if ( alphabet.indexOf(currentChar) === -1 ) specialChars[1]++;
+      else {
+        if ( !charMap[currentChar] ) {
+          console.log(currentChar);
+          return false;
+        }
+        charMap[currentChar]--;
+        if ( charMap[currentChar] === 0 ) delete charMap[currentChar];
+      }
+    }
+    if ( Object.keys(charMap).length > 0 ) return false;
+    //console.log(st)
+    return (stringA.length - specialChars[0]) === (stringB.length - specialChars[1])
+  }
+  
 
 module.exports = anagrams;
