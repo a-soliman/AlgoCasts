eaef5b2314aaf152c7bd597d76e56418a46194e9
a-soliman <ahmed.soliman@programmer.net>
2018-06-30 10:03:11 -0700

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let negative = n < 0 ? true : false;
+    let strN = n.toString();
+    let reversedStr = '';
+    let end = negative ? 1 : 0;
+    for ( let i = strN.length-1; i >= end; i-- ) {
+        reversedStr += strN[i];
+    }
+    return negative ? parseInt(-reversedStr) : parseInt(reversedStr);
+}
 
 module.exports = reverseInt;
