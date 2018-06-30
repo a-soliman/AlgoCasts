eae81dec7b436b885a9708cc9a1f344a2b724f82
a-soliman <ahmed.soliman@programmer.net>
2018-06-30 10:15:54 -0700

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+    let maxChar;
+    let frequency = 0;
+  
+    str.split('').forEach(char => {
+      if ( !chars[char] ) chars[char] = 0;
+      chars[char]++;
+    });
+  
+    for ( const key in chars ) {
+      if ( chars[key] > frequency ) {
+        frequency = chars[key];
+        maxChar = key;
+      }
+    }
+    return maxChar;
+  }
 
 module.exports = maxChar;
