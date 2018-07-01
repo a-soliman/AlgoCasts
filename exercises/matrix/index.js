769081e29273d7c2dd768714c56fe8aadf073e2e
a-soliman <ahmed.soliman@programmer.net>
2018-07-01 11:24:09 -0700

    spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,50 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let result = [];
+  
+    // build result array
+    for ( let i = 0; i < n; i++ ) {
+      let newRow = [];
+      for ( let j = 0; j < n; j++ ) {
+        newRow.push('');
+      }
+      result.push(newRow);
+    }
+  
+    let rowStart = 0, rowEnd = n-1;
+    let colStart = 0, colEnd = n-1;
+    let step = 1;
+  
+    while ( rowStart <= rowEnd && colStart <= colEnd ) {
+      for ( let i = colStart; i <= colEnd; i++ ) {
+        result[rowStart][i] = step;
+        step++;
+      }
+      rowStart++;
+  
+      for ( let i = rowStart; i <= rowEnd; i++ ) {
+        result[i][colEnd] = step;
+        step++;
+      }
+      colEnd--;
+  
+      for ( let i = colEnd; i >= colStart; i-- ) {
+        result[rowEnd][i] = step;
+        step++;
+      }
+      rowEnd--;
+  
+      for ( let i = rowEnd; i >= rowStart; i-- ) {
+        result[i][colStart] = step;
+        step++;
+      }
+      colStart++;
+  
+    }
+  
+    return result;
+  }
 
 module.exports = matrix;
