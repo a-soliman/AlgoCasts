a9cc5a74d194498f6f643a21b80ac25b70acc4d0
a-soliman <ahmed.soliman@programmer.net>
2018-06-30 19:50:12 -0700

    req. solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,20 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    for ( let row = 0; row < n; row++ ) {
-      let newLevel = '';
-  
-      for ( let col = 0; col < n *2 -1; col++ ) {
-        let mid = Math.floor((n *2 -1) /2);
-        let start = mid - row;
-        let end = mid + row;
-  
-        if ( col >= start && col <= end ) newLevel += '#';
-        else newLevel += ' ';
-      }
-      console.log(newLevel);
+function pyramid(n, row = 0, col='') {
+    if ( n === row ) return;
+    
+    if ( col.length === n *2 -1 ) {
+      console.log(col);
+      return pyramid(n, row+1);
     }
-}
+  
+    const mid = Math.floor((n * 2 -1) /2);
+    const start = mid - row;
+    const end = mid + row;
+  
+    if ( col.length >= start && col.length <= end ) col += '#';
+    else col += ' ';
+  
+    return pyramid(n, row, col);
+  
+  }
 
 module.exports = pyramid;
+
+
+
+// function pyramid(n) {
+//     for ( let row = 0; row < n; row++ ) {
+//       let newLevel = '';
+  
+//       for ( let col = 0; col < n *2 -1; col++ ) {
+//         let mid = Math.floor((n *2 -1) /2);
+//         let start = mid - row;
+//         let end = mid + row;
+  
+//         if ( col >= start && col <= end ) newLevel += '#';
+//         else newLevel += ' ';
+//       }
+//       console.log(newLevel);
+//     }
+// }
