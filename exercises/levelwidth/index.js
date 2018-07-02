cb3873b5ccb2a93bfd5483466a557a843f7f3908
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-07-02 12:24:29 -0700

    levelWidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root, level = 0, width = []) {
+    if ( !Array.isArray(root) ) root = [root];
+    width.push(root.length);
+    let newLevelNodes = [];
+    for ( let i = 0; i < root.length; i++ ) {
+      const node = root[i];
+  
+      if (node.children.length >  0) {
+        for ( let j = 0; j < node.children.length; j++ ) {
+          const child = node.children[j];
+          newLevelNodes.push(child);
+        }
+        
+      }
+    }
+    if ( newLevelNodes.length < 1 ) return width;
+    return levelWidth(newLevelNodes, level +1, width);
+    
+  }
 
 module.exports = levelWidth;
